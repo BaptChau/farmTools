c027eeb6f05f292dae8ccf5b5efe0bfc3c3a0042
Baptiste Chaudron <chaudron964@gmail.com>
2021-02-06 20:23:02 +0100

    Modification des forms

--- src/Form/MachineType.php
+++ src/Form/MachineType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Machine;
+use App\Entity\MachineType as EntityMachineType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,33 @@ class MachineType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('marque')
-            ->add('modele')
-            ->add('nb_heure')
-            ->add('type')
+            ->add('marque', TextType::class, [
+                'attr'=>[
+                    'class'=> 'form-control',
+                    'required'=>true
+                ],
+                'label'=> 'Libellé du type de la machine'
+            ])
+            ->add('modele', TextType::class,[
+                'attr'=>[
+                    'class'=> 'form-control',
+                    'required'=>true
+                ],
+                'label'=> 'Libellé du type de la machine'
+            ])
+            ->add('nb_heure', NumberType::class, [
+                'label'=> 'Nombre d\'heure de la machine',
+                'attr'=>[
+                    'class'=>'form-control',
+                    'required'=>true
+                ]
+            ])
+            ->add('type', EntityType::class,[
+                'class'=> EntityMachineType::class,
+                'choice_label'=>function(EntityMachineType $machineType){
+                    return sprintf('(%s) => %s', $machineType->getLibelle(),$machineType->getId());
+                }
+            ])
         ;
     }
 
--- src/Form/MachineTypeType.php
+++ src/Form/MachineTypeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\MachineType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,13 @@ class MachineTypeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('libelle')
+            ->add('libelle',TextType::class, [
+                'label'=>'Nom du type à ajouter :',
+                'attr'=> [
+                    'class'=>'form-control',
+                    'required'=>true
+                ]
+            ])
         ;
     }
 
